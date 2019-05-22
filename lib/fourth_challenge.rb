f51dcf58745a674734919cd783b139d1d3b8fd9d
Dan-Vy Le <danvyle85@gmail.com>
2019-05-22 00:39:40 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -14,8 +14,9 @@ def fourth_challenge
         name: "Mercutio",
         age: "18",
         attitude: "hot-headed"
-        }]
-    },
+        }
+        ]
+        },
     {
    :capulet => {
       :patriarch => {name: "Lord Capulet", age: "50"},
@@ -24,5 +25,6 @@ def fourth_challenge
       :heroine_friends => [
         {name: "Steven", age: "30", attitude: "confused"},
         {name: "Nurse", age: "44", attitude: "worried"}]
+    }
     }
 end
