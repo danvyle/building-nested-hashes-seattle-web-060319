ba9745fe531d302bbeee74c3372b0ce769eb745e
Dan-Vy Le <danvyle85@gmail.com>
2019-05-22 00:59:48 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -3,12 +3,12 @@ def fourth_challenge
    :montague => {
     :patriarch => {name: "Lord Montague", age: "53"},
     :matriarch => {name: "Lady Montague", age: "54"},
-    :hero => {name: "Romeo", age: "15", status: "alive"},
+    :hero => {name: "Romeo", age: "15", status:"alive"},
     :hero_friends => [
       {name: "Benvolio", age: "18", attitude: "worried"},
       {name: "Mercutio", age: "18", attitude: "hot -headed"}
       ]},
-    {
+
     :capulet => {
       :patriarch => {name: "Lord Capulet", age: "50"},
       :matriarch => {name: "Lady Capulet", age: "51"},
