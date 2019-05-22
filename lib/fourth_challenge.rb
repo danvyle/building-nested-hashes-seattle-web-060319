b493e6f121bf08a629488108b91e29ff887f401a
Dan-Vy Le <danvyle85@gmail.com>
2019-05-22 00:33:12 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -23,8 +23,9 @@ def fourth_challenge
       :heroine => {name: "Juliet", age: "15", status: "alive"},
       :heroine_friends => [
         {
-        name
-        #your hashes here!
+        name: "Steven", age: "30", attitude: "confused"},
+        {name: "Nurse", age: "44", attitude: "worried"}
+        
       ]
    }
 }
